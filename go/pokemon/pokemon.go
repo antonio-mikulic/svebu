48fc2c73cdb52312e9021cef6bb445b8d0059f98
Antonio Mikulić <antonio.mikulic.work@outlook.com>
2023-09-10 13:10:45 +0200

    add error handling in concurrent request

--- go/pokemon/pokemon.go
+++ go/pokemon/pokemon.go
@@ -25,7 +25,7 @@ func getRandomPokemonForUser(w http.ResponseWriter, r *http.Request) {
 	start := time.Now()
 
 	pokemonList := getRandomPokemon()
-	pokemonData := make(chan pokemon.PokemonData, len(pokemonList))
+	pokemonData := make(chan PokemonDetailsChannelHolder, len(pokemonList))
 	wg := &sync.WaitGroup{}
 	wg.Add(len(pokemonList))
 
@@ -36,9 +36,12 @@ func getRandomPokemonForUser(w http.ResponseWriter, r *http.Request) {
 	wg.Wait()
 	close(pokemonData)
 
-	result := make([]pokemon.PokemonData, len(pokemonList))
+	result := make([]pokemon.PokemonData, 0)
 	for i := 0; i < len(pokemonList); i++ {
-		result[i] = <-pokemonData
+		pokemon := <-pokemonData
+		if pokemon.IsSuccessful {
+			result = append(result, pokemon.PokemonDetails)
+		}
 	}
 
 	fmt.Println("Time taken to get all pokemon details: ", time.Since(start))
@@ -77,24 +80,50 @@ func enableCors(w *http.ResponseWriter) {
 	(*w).Header().Set("Access-Control-Allow-Origin", "*")
 }
 
-func getPokemonDetails(pokemonName string, chn chan pokemon.PokemonData, wg *sync.WaitGroup) {
+func getPokemonDetails(pokemonName string, chn chan PokemonDetailsChannelHolder, wg *sync.WaitGroup) {
 	start := time.Now()
 	response, err := http.Get("http://pokeapi.co/api/v2/pokemon/" + pokemonName)
 
 	if err != nil {
-		fmt.Print(err.Error())
-		os.Exit(1)
+		chn <- PokemonDetailsChannelHolder{Name: pokemonName, IsSuccessful: false}
+		log.Default().Printf("Error getting pokemon details for %s: %s", pokemonName, err.Error())
+		wg.Done()
+		return
 	}
 
-	responseData, err := io.ReadAll(response.Body)
-	if err != nil {
-		log.Fatal(err)
+	responseData, ioErr := io.ReadAll(response.Body)
+	if ioErr != nil {
+		chn <- PokemonDetailsChannelHolder{Name: pokemonName, IsSuccessful: false}
+		log.Default().Printf("Error reading pokemon details for %s: %s", pokemonName, ioErr.Error())
+		wg.Done()
+		return
 	}
 
 	var responseObject pokemon.PokemonData
 	json.Unmarshal(responseData, &responseObject)
 
-	chn <- responseObject
+	if responseObject.Name == "" {
+		chn <- PokemonDetailsChannelHolder{Name: pokemonName, IsSuccessful: false}
+		log.Default().Printf("Error unmarshalling pokemon details for %s", pokemonName)
+		wg.Done()
+		return
+	}
+
+	if responseObject.ID > 136 {
+		chn <- PokemonDetailsChannelHolder{Name: pokemonName, IsSuccessful: false}
+		log.Default().Printf("%s is not recommended to new players", pokemonName)
+		wg.Done()
+		return
+	}
+
+	chn <- PokemonDetailsChannelHolder{Name: pokemonName, IsSuccessful: true, PokemonDetails: responseObject}
 	wg.Done()
+
 	fmt.Println("Time taken to get pokemon details for ", pokemonName, " : ", time.Since(start))
+}
+
+type PokemonDetailsChannelHolder struct {
+	Name           string
+	IsSuccessful   bool
+	PokemonDetails pokemon.PokemonData
 }
